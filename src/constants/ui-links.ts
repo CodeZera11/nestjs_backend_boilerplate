cab2dbd3d5e9f370a7bd0dd19ec0e2dd5a559a8e
codezera <codezera3@gmail.com>
2024-02-15 17:40:24 +0530

    added ui page links constant file

---
+++ src/constants/ui-links.ts
@@ -0,0 +1,6 @@
+const BASE_URL = process.env.UI_BASE_URL || 'https://fortune-ui.vercel.app';
+export const UiPageLinks = {
+  SignInPage: `${BASE_URL}/signin/`,
+  SignUpPage: `${BASE_URL}/signup/`,
+  ResetPasswordPage: `${BASE_URL}/reset-password/`,
+};
